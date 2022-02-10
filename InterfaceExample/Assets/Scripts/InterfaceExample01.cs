b4da2d25f2e9d1b180d0ac55a39a3b1719238d13
eungyukm <eungyukm@gmail.com>
2022-02-10 09:36:45 +0900

    인터페이스 추가

--- InterfaceExample/Assets/Scripts/InterfaceExample01.cs
+++ InterfaceExample/Assets/Scripts/InterfaceExample01.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InterfaceExample01 : MonoBehaviour
 {
@@ -9,45 +9,65 @@ public class InterfaceExample01 : MonoBehaviour
         hydralisk.Burrow();
         hydralisk.RecoveryHp();
         // Log
-        // ���ӿ� �����ϴ�.
-        // ü���� ����մϴ�.
+        // 땅속에 숨습니다.
+        // 체력을 재생합니다.
 
-        IZerg iZerg = new Hydralisk(); // iZerg�� ����󸮽�ũ�� �ν��Ͻ�
+        IZerg iZerg = new Hydralisk(); // iZerg는 히드라리스크의 인스턴스
         iZerg.Burrow();
         iZerg.RecoveryHp();
         // Log
-        // ���ӿ� �����ϴ�.
-        // ü���� ����մϴ�.
+        // 땅속에 숨습니다.
+        // 체력을 재생합니다.
+
+        IMetamorph metamorph = new Hydralisk();
+        metamorph.Morph("러커");
+
+        // 러커로 변형 됩니다.
+        // Debug.Log(metamorph.damage); // 접근 불가 해당 타입에 정의되어 있지 않기 때문
+
+        // 다운 캐스팅
+        int hydra = ((Hydralisk)hydralisk).damage;
+        Debug.Log("hydra : " + hydra);
+        // hydra : 3
+
+        Hydralisk heroHydralisk = new Hydralisk();
+        heroHydralisk.Morph("러커");
+        heroHydralisk.Burrow();
+        heroHydralisk.RecoveryHp();
+        Debug.Log("Hero Hydralisk Damage : " + heroHydralisk.damage);
+        // Hero Hydralisk Damage : 3
     }
 }
 
 interface IZerg
 {
-    // ���� �����ڸ� ����� �� �����ϴ�.
+    // 접근 제한자를 사용할 수 없습니다.
     void RecoveryHp();
     void Burrow();
 }
 
-// �Ļ� Ŭ���������� �ݵ�� �������̽����� ���ǵ� �޼��带 �����ؾ� �մϴ�.
+// 파생 클래스에서는 반드시 인터페이스에서 정의된 메서드를 구현해야 합니다.
 class Hydralisk : IZerg, IMetamorph
 {
+    public int damage = 3;
+
     public void Burrow()
     {
-        Debug.Log("���ӿ� �����ϴ�");
+        Debug.Log("땅속에 숨습니다");
     }
 
     public void RecoveryHp()
     {
-        Debug.Log("ü���� ����մϴ�.");
+        Debug.Log("체력을 재생합니다.");
     }
 
     public void Morph(string name)
     {
-        Debug.Log("���� �˴ϴ�.");
+        Debug.Log(name + "으로 변형 됩니다.");
     }
 }
 
 interface IMetamorph
 {
-    void Morph(string name); // ���� ex) ��� -> ���۸�, ����󸮽�ũ -> ��Ŀ
+    void Morph(string name); // 변형 ex) 드론 -> 저글링, 히드라리스크 -> 러커
 }
